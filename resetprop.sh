5174d00e8f85141a4f20550d07268e0e276c1d34
Júnior Almeida <jun10r4lm31d4@outlook.com>
2024-10-07 11:57:43 -0300

    Update resetprop.sh

--- resetprop.sh
+++ resetprop.sh
@@ -14,3 +14,9 @@ replace_value_resetprop(){
     local VALUE_NEW="$(echo -n "$VALUE" | sed "s|${2}|${3}|g")"
     [ "$VALUE" == "$VALUE_NEW" ] || resetprop -v -n "$1" "$VALUE_NEW"
 }
+
+delete_prop_with_name_resetprop(){
+    for PROP in $(resetprop | grep "$1" | cut -d ';' -f1 | cut -d '[' -f2 | cut -d ']' -f1); do
+        resetprop -d $PROP
+    done
+}
